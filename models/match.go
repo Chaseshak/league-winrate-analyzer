5f35546e9d7a14858b2f1830c5c050095731d8a2
Chase Schachenman <chaseshak@gmail.com>
2023-10-30 16:17:13 -0500

    Parse relevant match information

--- models/match.go
+++ models/match.go
@@ -1,4 +1,26 @@
 package models
 
+type TeamPosition string
+
+const (
+	Top     TeamPosition = "TOP"
+	Jungle  TeamPosition = "JUNGLE"
+	Mid     TeamPosition = "MID"
+	Bot     TeamPosition = "BOT"
+	Support TeamPosition = "SUPPORT"
+)
+
 type Match struct {
+	Info MatchInfo `json:"info"`
+}
+
+type MatchInfo struct {
+	Participants []Participant `json:"participants"`
+}
+
+type Participant struct {
+	ChampionName string       `json:"championName"`
+	Puuid        string       `json:"puuid"`
+	TeamPosition TeamPosition `json:"teamPosition"`
+	Win          bool         `json:"win"`
 }
